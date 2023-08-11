e29d783da60fde51bc14e37ce64e520cc4873e4f
Vitorrrrrrrrr <vitoravs008@gmail.com>
2023-08-11 13:55:10 -0300

    ijhoiuh

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ function criar() {
 
     var info = [
         document.getElementById('nome').value,
-        $('#classe').val,
-        $('#servidor').val,
-        $('#cidade').val,
         document.getElementById('email').value,
         document.getElementById('senha').value
     ]
 
     console.log(info);
-    let sess = [document.getElementById('nome-aws'), document.getElementById('classe-aws'), document.getElementById('serv-aws')]
-    sess[0].innerHTML = `${info[0]}`;
+
+    document.getElementById("nome-aws").textContent = `<span>${info}</span>`;
 }
+
