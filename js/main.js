cdd5827d37fa950459dad97b6ad07b9f4ad74e48
Vitorrrrrrrrr <vitoravs008@gmail.com>
2023-08-11 13:26:00 -0300

    jhbjh

--- js/main.js
+++ js/main.js
@@ -1,22 +1,15 @@
 function criar() {
-    var classe = document.getElementById('classe').value;
-    var server = document.getElementById('servidor').value;
-    var cidade = document.getElementById('cidade').value;
-
 
     var info = [
-        document.getElementById('nome'),
-        classe,
-        server,
-        cidade,
-        document.getElementById('email'),
-        document.getElementById('senha')
+        document.getElementById('nome').value,
+        $('#classe').val,
+        $('#servidor').val,
+        $('#cidade').val,
+        document.getElementById('email').value,
+        document.getElementById('senha').value
     ]
 
     console.log(info);
     let sess = [document.getElementById('nome-aws'), document.getElementById('classe-aws'), document.getElementById('serv-aws')]
-
-    for (i = 0; i < 2; i++) {
-        sess[i].innerHTML = `${info[i]}`;
-    }
+    sess[0].innerHTML = `${info[0]}`;
 }
